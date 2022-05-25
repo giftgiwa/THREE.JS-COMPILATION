03034892ec6845e2bf47d168e9ce933f7c0ed8d5
GiftGiwa <gdg2131@columbia.edu>
2022-05-25 14:11:26 -0400

    Update source.js

--- Sphere/source.js
+++ Sphere/source.js
@@ -1,13 +1,13 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-//import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js'
 import { RGBELoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/RGBELoader.js';
 
 //starter
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = -60;
+camera.position.z = -30;
 camera.position.y = 6;
 
 const renderer = new THREE.WebGLRenderer();
@@ -18,7 +18,7 @@ renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 //orbit controls
-//let controls = new OrbitControls(camera, renderer.domElement)
+let controls = new OrbitControls(camera, renderer.domElement)
 
 //HDRI MAP
 const hdrEquirect = new RGBELoader().load(
@@ -33,68 +33,81 @@ const sphereMaterial = new THREE.MeshPhysicalMaterial({color: 0xdce0e6,
 const equatorMaterial = new THREE.MeshStandardMaterial({color: 0x242526, metalness:1, 
 	opacity:0.8, roughness:0.8} )
 
-const loader = new GLTFLoader();
-                                                                                                                  
-let all = [...Array(12)].map(e => Array(2).fill(new THREE.Mesh())) //array of spheres/equators
-console.log(all)
+const loader = new GLTFLoader();                                                                                        
 
-let positions = [] 
+let ball;
+let geometry;
 
-for (let i = 0; i < 12 ; i++) {
-	
-	let scale = Math.floor(Math.random() * 3) + 1
-	let pos_x = Math.floor(Math.random() * 9) * 5 - 25
-	let pos_y = Math.floor(Math.random() * 9) * 5 - 25
-	let pos_z = Math.floor(Math.random() * 9) * 5 - 25
-
-	loader.load( './ball.gltf', function ( gltf ) { //load sphere
-		gltf.scene.traverse(function(model) {
-			if (model.isMesh) {
-				model.castShadow = true;
-				model.material = sphereMaterial;
-			}
-		});
-
-		let pos = new THREE.Vector3(pos_x, pos_y, pos_z)
-		positions.push(pos.distanceTo(new THREE.Vector3(0, pos.y , 0))) //log of distances between meshes and y-axis
-
-		all[i][0] = gltf.scene
-		all[i][0].position.set(pos_x, pos_y, pos_z)
-		all[i][0].scale.set(scale, scale, scale)
-
-		scene.add( all[i][0] )	
-	}, undefined, function ( error ) {
-		console.error( error );
-	} );
-
-	loader.load( './equator.gltf', function ( gltf ) { //load sphere
-		gltf.scene.traverse(function(model) {
-			if (model.isMesh) {
+loader.load( './ball.gltf', function ( gltf ) { //load sphere
+	gltf.scene.traverse(function(model) {
+		if (model.isMesh) {
 			model.castShadow = true;
-			model.material = equatorMaterial;
-			}
-		});
-		
-		all[i][1] = gltf.scene
-		all[i][1].position.set(pos_x, pos_y, pos_z)
-		all[i][1].scale.set(scale, scale, scale)
+			model.material = sphereMaterial;
+			
+		}
+	});
 
-		scene.add( all[i][1] )	
-	}, undefined, function ( error ) {
-		console.error( error );
-	} );
+	ball = gltf.scene
+	ball.scale.set(5, 5, 5)
+	geometry = ball.geometry
+	
+	//scene.add( ball )	
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+
+
+const randomizeMatrix = function () {
+
+	const position = new THREE.Vector3();
+	const rotation = new THREE.Euler();
+	const quaternion = new THREE.Quaternion();
+	const scale = new THREE.Vector3();
+
+	return function ( matrix ) {
+
+		position.x = Math.random() * 40 - 20;
+		position.y = Math.random() * 40 - 20;
+		position.z = Math.random() * 40 - 20;
+
+		rotation.x = Math.random() * 2 * Math.PI;
+		rotation.y = Math.random() * 2 * Math.PI;
+		rotation.z = Math.random() * 2 * Math.PI;
+
+		quaternion.setFromEuler( rotation );
+
+		scale.x = scale.y = scale.z = Math.random() * 1;
+
+		matrix.compose( position, quaternion, scale );
+
+	};
+
+}();
+
+
+
+const mesh = new THREE.InstancedMesh(geometry, sphereMaterial, 10)
+const matrix = new THREE.Matrix4()
+
+for ( let i = 0; i < 10; i ++ ) {
+
+	randomizeMatrix( matrix );
+	mesh.setMatrixAt( i, matrix );
+
 }
+
+scene.add(mesh);
 
 //light
 const light = new THREE.AmbientLight( 0xffffff );
 scene.add( light );
 
-
 //render
 function animate() {
 	requestAnimationFrame( animate );
 	//camera.lookAt(0, 0, 60)
-	for (let j = 0; j < all.length; j++) {
+	
+	/*for (let j = 0; j < all.length; j++) {
 		let rot_x = Math.random() * 0.00005 - 0.2
 		let rot_z = Math.random() * 0.00005 - 0.2
 		let movement = Math.random() * 0.2
@@ -109,7 +122,8 @@ function animate() {
 			all[j][0].position.y = -50
 			all[j][1].position.y = -50
 		}
-	}
+	}*/
+
 	//matrix.makeRotationY(clock.getDelta() * 2 * Math.PI / period);
 	//camera.position.applyMatrix4(matrix);	
 	//camera.lookAt(0, 0, 0);	
