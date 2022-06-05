be00ef5da56a78767aaecd96a97f7c978c5d3580
GiftGiwa <gdg2131@columbia.edu>
2022-06-05 18:18:05 -0400

    file change

--- Sphere/source.js
+++ Sphere/source.js
@@ -12,13 +12,13 @@ camera.position.y = 6;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio( window.devicePixelRatio * 2);
+renderer.setPixelRatio( window.devicePixelRatio * 4);
 document.body.appendChild( renderer.domElement );
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 //orbit controls
-let controls = new OrbitControls(camera, renderer.domElement)
+//let controls = new OrbitControls(camera, renderer.domElement)
 
 //HDRI MAP
 const hdrEquirect = new RGBELoader().load(
@@ -30,56 +30,48 @@ const hdrEquirect = new RGBELoader().load(
 const sphereMaterial = new THREE.MeshPhysicalMaterial({color: 0xdce0e6, 
 	metalness:0, opacity:1.0, transmission:1, envMap: hdrEquirect, 
 	roughness:0, depthTest: true} )
-const equatorMaterial = new THREE.MeshStandardMaterial({color: 0x242526, metalness:1, 
-	opacity:0.8, roughness:0.8} )
+/*const equatorMaterial = new THREE.MeshStandardMaterial({color: 0x242526, metalness:1, 
+	opacity:0.8, roughness:0.8} )*/
 
-const loader = new GLTFLoader();
+const manager = new THREE.LoadingManager();
+const loader = new GLTFLoader(manager);
                                                                                                                   
-let all = [...Array(12)].map(e => Array(2).fill(new THREE.Mesh())) //array of spheres/equators
+let all = [...Array(12)].map((e) => new THREE.Mesh()) //array of spheres/equators
 console.log(all)
 
 let positions = [] 
 
+manager.onLoad = (e) => {
+	console.log("loading complete!")
+	
+}
+
 for (let i = 0; i < 12 ; i++) {
 	
 	let scale = Math.floor(Math.random() * 3) + 1
-	let pos_x = Math.floor(Math.random() * 9) * 5 - 25
-	let pos_y = Math.floor(Math.random() * 9) * 5 - 25
-	let pos_z = Math.floor(Math.random() * 9) * 5 - 25
+	let pos_x = Math.floor(Math.random() * 9) * 4 - 20
+	let pos_y = Math.floor(Math.random() * 9) * 4 - 20
+	let pos_z = Math.floor(Math.random() * 9) * 4 - 20
 
 	loader.load( './ball.gltf', function ( gltf ) { //load sphere
 		gltf.scene.traverse(function(model) {
 			if (model.isMesh) {
 				model.castShadow = true;
-				model.material = sphereMaterial;
 			}
 		});
 
-		let pos = new THREE.Vector3(pos_x, pos_y, pos_z)
-		positions.push(pos.distanceTo(new THREE.Vector3(0, pos.y , 0))) //log of distances between meshes and y-axis
+		gltf.scene.children[0].material = sphereMaterial
 
-		all[i][0] = gltf.scene
-		all[i][0].position.set(pos_x, pos_y, pos_z)
-		all[i][0].scale.set(scale, scale, scale)
+		all[i] = gltf.scene
+		all[i].position.set(pos_x, pos_y, pos_z)
+		all[i].scale.set(scale, scale, scale)
 
-		scene.add( all[i][0] )	
-	}, undefined, function ( error ) {
-		console.error( error );
-	} );
+		//console.log(all[i].position)
+		positions.push(all[i].position.distanceTo(new THREE.Vector3(0, all[i].position.y, 0))) 
+		//^log of distances between meshes and y-axis
 
-	loader.load( './equator.gltf', function ( gltf ) { //load sphere
-		gltf.scene.traverse(function(model) {
-			if (model.isMesh) {
-			model.castShadow = true;
-			model.material = equatorMaterial;
-			}
-		});
+		scene.add( all[i] )	
 		
-		all[i][1] = gltf.scene
-		all[i][1].position.set(pos_x, pos_y, pos_z)
-		all[i][1].scale.set(scale, scale, scale)
-
-		scene.add( all[i][1] )	
 	}, undefined, function ( error ) {
 		console.error( error );
 	} );
@@ -89,32 +81,33 @@ for (let i = 0; i < 12 ; i++) {
 const light = new THREE.AmbientLight( 0xffffff );
 scene.add( light );
 
-console.log(all[0][0].position)
-
 
 //render
 function animate() {
-	requestAnimationFrame( animate );
-	//camera.lookAt(0, 0, 60)
 
-	/*let rot_x = Math.random() * 0.00005 - 0.002
+	let x = 0
+	let z = 0
+
+	requestAnimationFrame( animate );
+	camera.lookAt(0, 0, 60)
+
+	x += 0.01
+	z += 0.01
+	let rot_x = Math.random() * 0.00005 - 0.002
 	let rot_z = Math.random() * 0.00005 - 0.002
 		
 	for (let j = 0; j < all.length; j++) {
-		let movement = Math.random() * 0.7
-		all[j][0].position.y += movement
-		all[j][1].position.y += movement
+		let movement = Math.random() * 0.1
+		//all[j].position.y += movement
 
-		//all[j][1].rotation.x += rot_x
-		//all[j][1].rotation.z += rot_z
-		all[j][1].rotation.y += 0.05
-		
+		//all[j].rotation.x += rot_x
+		//all[j].rotation.z += rot_z
+		all[j].rotation.y += 0.05	
 
-		if (all[j][0].position.y >= 50) {
-			all[j][0].position.y = -50
-			all[j][1].position.y = -50
+		if (all[j].position.y >= 30) {
+			all[j].position.y = -30
 		}
-	}*/
+	}
 
 	
 	
