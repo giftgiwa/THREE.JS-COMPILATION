48cfbcfff91036a69305e255c9e8ecfd89ea8b74
GiftGiwa <gdg2131@columbia.edu>
2022-11-17 16:14:12 -0500

    Update sphere_animation.js

--- Sphere/sphere_background/sphere_animation.js
+++ Sphere/sphere_background/sphere_animation.js
@@ -1,13 +1,17 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-//import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js'
 import { RGBELoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/RGBELoader.js';
 
 
 //delay set to make time for startup animation
 //setTimeout(function() {three()}, 2000)
+let time = 0.00
+
 three()
+
 function three() {
+
 	const scene = new THREE.Scene();
 	//scene.background = new THREE.Color(0xf7f7f7);
 	const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -18,30 +22,40 @@ function three() {
 	camera.position.y = 6;
 	camera.lookAt(0, 0, 100)
 
-	const renderer = new THREE.WebGLRenderer({alpha: true } );
-	renderer.setSize( /*0.666 * */ window.innerWidth, /*0.666 * */window.innerHeight);
+	const renderer = new THREE.WebGLRenderer({alpha: true }) // {alpha: true} makes the background transparent
+
+	renderer.setSize( /*0.666 * */ window.innerWidth, /*0.666 * */window.innerHeight)
 	renderer.setPixelRatio( window.devicePixelRatio);
+	
 	//document.body.appendChild( renderer.domElement );
-	document.getElementById('canvas').appendChild( renderer.domElement );
+	document.getElementById('canvas').appendChild( renderer.domElement )
 
 	//HDRI MAP
 	const hdrEquirect = new RGBELoader().load(
-		"sphere_background/paul_lobe_haus_4k.hdr",  
+		"./sphere_background/paul_lobe_haus_4k.hdr",  
 		() => { hdrEquirect.mapping = THREE.EquirectangularReflectionMapping; }
 	);
 
 	//orbit controls
-	//const controls = new OrbitControls( camera, renderer.domElement );
+	const controls = new OrbitControls( camera, renderer.domElement );
 
 	//materials
-	const sphereMaterial = new THREE.MeshPhysicalMaterial({color: 0xdce0e6, 
-		metalness:0, opacity:1.0, transmission:1, envMap: hdrEquirect, 
-		roughness:0, depthTest: true} )
-	const sphereMaterial_2 = new THREE.MeshBasicMaterial({color: 0x000000/*, metalness:1, 
-	opacity:0.8, roughness:0.8*/} )
+	const sphereMaterial = new THREE.MeshPhysicalMaterial({
+		color: 0xffffff, 
+		transmission:1,
+		envMap: hdrEquirect, 
+		roughness:0, 
+		depthTest: true
+	})
+
+	const sphereMaterial_2 = new THREE.MeshBasicMaterial({
+		color: 0x000000,
+		metalness:1, 
+		opacity:0.8, 
+		roughness:0.8
+	})
 
 	const loader = new GLTFLoader()
-
 
 	const randomizeMatrix = function () { //randomize location and size for each instance of the sphere
 
@@ -66,13 +80,12 @@ function three() {
 			matrix.compose( position, quaternion, scale )
 
 		};
-
 	}();
 
 	let mesh //mesh to become the InstancedMesh
 	let mesh_2
 
-	const count = 40 //number of instances to be made
+	const count = 50 //number of instances to be made
 
 	//animation variables (for end of code)
 
@@ -94,7 +107,7 @@ function three() {
 		
 			const matrix = new THREE.Matrix4() //not the same as matrix defined above!
 
-			mesh = new THREE.InstancedMesh( gltf.scene.children[0].geometry, sphereMaterial_2, count )
+			mesh = new THREE.InstancedMesh( gltf.scene.children[0].geometry, sphereMaterial, count )
 			mesh.instanceMatrix.setUsage( THREE.DynamicDrawUsage )
 
 			mesh_2 = new THREE.InstancedMesh( gltf.scene.children[1].geometry, gltf.scene.children[1].material, count )
@@ -123,8 +136,10 @@ function three() {
 	function animate() {
 
 		requestAnimationFrame( animate )
+		//console.log(clock.getDelta());
+		time += 0.00001
 	
-		matrix.makeRotationY(clock.getDelta() * 2 * Math.PI / period)
+		matrix.makeRotationY(0.025 * 2 * Math.PI / period)
 
 		camera.lookAt(new THREE.Vector3(0, 0, 0)) //keep camera looking at center of scene
 		camera.position.applyMatrix4(matrix)
